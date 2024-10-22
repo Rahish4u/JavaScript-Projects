9bb24ffa9c5a0bebb840b7784d73c9b407197722
Rahish4u <rahishali0591@gmail.com>
2024-10-22 13:11:43 +0530

    Make a simple Counter app using HTML, CSS and JavaScript

---
+++ DOM Demo/index.js
@@ -0,0 +1,14 @@
+// document.getElementById("heading2");
+// console.dir(heading2)
+
+// let heading = document.getElementsByClassName("heading");
+// console.dir(heading)
+
+// let parah = document.getElementsByTagName("p");
+// console.dir(parah)
+
+let firstEl = document.querySelector("p")
+console.dir(firstEl)
+
+let allEl = document.querySelectorAll("p")
+console.dir(allEl)
